c768baca8bddfdbc4660abe053702cbd8a5f29f7
= <=>
2014-10-22 21:27:55 -0400

    named the plot wrongly

--- plot1.R
+++ plot1.R
@@ -6,7 +6,7 @@
 dataFrame <- subset(read.table(file="household_power_consumption.txt", sep=";", header=TRUE, na.strings="?"), Date=="1/2/2007" | Date=="2/2/2007")
 
 # Plot the histogram to a png file
-png(file = "plot11.png", width = 480, height = 480)
+png(file = "plot1.png", width = 480, height = 480)
 hist(dataFrame$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
 
 # Shut down the current device
